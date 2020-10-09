40ce9173c8d7c3b784755fd0f9abceeb5a818c21
divcraft <t.ogonowski@wp.pl>
2020-10-09 23:13:48 +0200

    update navbar content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,7 @@ const Navbar = () => {
                <li>Home</li>
                <li>About</li>
                <li>Gallery</li>
+               <li>Products</li>
                <li>Contact</li>
             </ul>
          </nav>
